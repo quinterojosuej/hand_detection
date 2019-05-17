15578d996e97786b9829386397cae89236c00544
quinterojosuej <quinterojosuej>
2019-05-17 00:33:48 -0700

    finished commenting on linux version

--- linuxObjectDetection.cpp
+++ linuxObjectDetection.cpp
@@ -1,3 +1,10 @@
+/*
+Created by Jose Caudillo, Brenda Jacobo, Kevin Piffero and Jonathan Quintero
+Date: 5/18/19
+Hand Detection program that allows for a specified gesture, see video or gifs, 
+to open certain programs already present in the system. Using OpenCV and System 
+commands on c++.
+*/
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -21,7 +28,6 @@ int main( int argc, const char** argv )
 {
 
     //-- 1. Load the cascades
-    ///if( !face_cascade.load( face_cascade_name ) )
 
     if( !hand_cascade.load( "aGest.xml" ) )
     {
@@ -29,7 +35,6 @@ int main( int argc, const char** argv )
         return -1;
     };
 
-    //int camera_device = parser.get<int>("camera");
     VideoCapture capture;
     
     //-- 2. Read the video stream
@@ -61,21 +66,25 @@ int main( int argc, const char** argv )
     return 0;
 }
 
-///updatable variables
-
-///int initial_x_detected;
+///This vector is for duplicating the first hand detections
 std::vector<Rect> initial_x_detected;
 
-
+///is_initial_detected is for finding first objects
 bool is_initial_detected = false;
+///the following variables store the locations of the second detected objects
 int secondary_x_detected;
 int secondary_y_detected;
 
+///horizontal_shift is used to verify that there was a horizontal shift
 bool horizontal_shift = false;
+///the following variables store the data location of the final detection 
 int tertiary_x_detected;
 int tertiary_y_detected;
 
-///function usage
+/// The function obtains the data regarding two hands detected
+// and determines whether or not there was a shift on the corresponding hands
+// in regards to the location of the hand. If there was a true horizontal shift
+// then horizontal_shift = true, else horizontal_shift = false.
 void second_object_shift_correctly(int first_x, int second_x, int frame_size)
 {
 
@@ -103,15 +112,18 @@ void second_object_shift_correctly(int first_x, int second_x, int frame_size)
     }
 }
 
-///function usage
+/// The following function activates the corresponding app on whether the last
+// location of the final activation was on either the left or right side of the 
+// total screen
 void corresponding_program(int third_x, int framewidth)
 {
-	// for the left it is firefox, for the right it is terminal
+	// for the left it is firefox 
 	if( third_x < framewidth *.5 )
 	{
 		system("firefox");
 		cout << "FIREFOX"<< endl;
 	}
+	// for the right it is terminal
 	else
 	{
 		system("gnome-terminal");
@@ -120,7 +132,9 @@ void corresponding_program(int third_x, int framewidth)
 }
 
 
-///function usage
+/// The following function determines whether there was a vertical shift after
+// the horizontal shift was confirmed. If there was a shift with a definite movement
+// then the corresponding_program function gets called. 
 void third_object_shift_correctly(int second_y, int third_y, int frameheight, int framewidth, int third_x)
 {
 	if(third_y > second_y + frameheight * .1)
@@ -128,53 +142,74 @@ void third_object_shift_correctly(int second_y, int third_y, int frameheight, in
 		corresponding_program( third_x, framewidth);
 	}
 }
-///function usage
+
+/// Simply returns the value of the duplicated vector's data of hand-objects detected
+// makes more on an eye pleasing look.
 int initial_looper(int the_counter)
 {
 	return(initial_x_detected[the_counter].x);
 }
 
-
-
+/// This counter deals with the fact that the frames only
+// detect hand objects every 10 frames. This just 
+// initializes it
 int counter = 0;
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
     Mat frame_gray;
+    // Allows for color change to occur and aid the detection process
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    ///equalizeHist( frame_gray, frame_gray );
+    /// equalizeHist( frame_gray, frame_gray ); // If used then detection rate decreases
 
-    //-- Detect
+    //The following places the frame's data into a vector
     std::vector<Rect> gests;
+    //This function determines whether an object is present or not and pushes to the vector gests
     hand_cascade.detectMultiScale( frame_gray, gests, 1.1, 0, 0, Size(30,30), Size(300,300) );
     if(counter == 10)
     {
 	counter = 0;
 	cout << "~~~~~~~~~~~~~~~~~~~~~~";
     }
+    //The counter is printed to show the rate at which detection occurs
+    //otherwise is optional. THE COUNTER IS NOT OPTIONAL.
     cout << counter << endl;
     counter++;
 
+    //The for loop uses the vector to cycle through all of the detections
     for ( size_t i = 0; i < gests.size(); i++ )
     {
+	//For development a box is created around the hand-obect that are detected
         Point center( gests[i].x + gests[i].width/2, gests[i].y + gests[i].height/2 );
-
+	
+	//This if allows for a 'death zone' to not have beards or shadws activate the program
 	if(gests[i].x < .33 *frame_gray.size().width || gests[i].x > .66 *frame_gray.size().width)
 	{
-//	cout << gests[i].x << "   ,    " << gests[i].y << endl;
-//	cout << gests[i].size() << "-------------" << endl;
 	
+	// This if allows for only frame data of every 10 frames to be used	
 	if(counter == 1){	
+	
+	//	cout << gests[i].x << "   ,    " << gests[i].y << endl; 
+	// for development purposes, prints coordinates of the objects
+	//      cout << gests[i].size() << "-------------" << endl;
+	
+	//This will create a rectangle around the hand-objects created
 	rectangle ( frame, gests[i], Scalar( 255, 0, 0 ), 5, 8, 0);
 
+	//If there is a confirmed horizontal_shift then a new objects are 
+	//tried to see if a confirmed third_object_shift_correctly
+	//occurs
 	if(horizontal_shift)
 	{
 		tertiary_x_detected = gests[i].x;
 		tertiary_y_detected = gests[i].y;
 		third_object_shift_correctly( secondary_y_detected , tertiary_y_detected, frame.size().height, frame.size().width, tertiary_x_detected );
 	}
-	
+	//If there was an initial objects than have the new detections
+	//be compared with the initial detections. If so then horizontal_shift = true
+	//making it so that the loop stops. Maintains the values for those that made 
+	//the horizontal_shift = true. Else keeps it as false.
 	if(is_initial_detected)
 	{
 		secondary_x_detected = gests[i].x;
@@ -193,8 +228,9 @@ void detectAndDisplay( Mat frame )
 		cout << "FOUND SECOND HAND!";
 	}
 
-
-	///this is the last possible thing
+	///The last possible event is that there were objects detected. If 
+	//there were, then they are stored in another vector for comparison
+	//in the horizontal shift 
 	if(i==0)
 	{
 	initial_x_detected = gests;
@@ -206,7 +242,8 @@ void detectAndDisplay( Mat frame )
     }
     }
 
-    //-- Display camera feed and hand detections
+    //Display camera feed and hand detections, really optional. Just needed for 
+    //demos and development.
     imshow( "Capture - Hand detection", frame );
 }
 
